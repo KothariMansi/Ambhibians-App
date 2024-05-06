4dd7ad1661e9a4269e263be754d99176d9dafd85
SHREE <mansikothari46@gmail.com>
2024-05-05 23:01:33 -0700

    Adding Loading and Error Screen

--- app/src/main/java/com/example/amphibiansapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/amphibiansapp/ui/screens/HomeScreen.kt
@@ -1,25 +1,31 @@
 package com.example.amphibiansapp.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
+import com.example.amphibiansapp.R
 import com.example.amphibiansapp.network.AmphibiansPhoto
 import com.example.amphibiansapp.ui.theme.AmphibiansAppTheme
 
@@ -31,10 +37,41 @@ fun HomeScreen(
 ) {
     when (amphibiansUiState) {
         is AmphibiansUiState.Success -> AmphibiansListScreen(amphibiansPhotoList = amphibiansUiState.photos)
-        AmphibiansUiState.Error -> {}
-        AmphibiansUiState.Loading -> {}
+        AmphibiansUiState.Error -> {
+            ErrorScreen(modifier = modifier.fillMaxSize())}
+        AmphibiansUiState.Loading -> {
+            LoadingScreen(
+                modifier = modifier.fillMaxWidth()
+            )}
     }
     
+}
+@Composable
+fun ErrorScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize()
+
+    ) {
+        Image(painter = painterResource(id = R.drawable.ic_connection_error),
+            contentDescription = "",
+        )
+    }
+
+}
+
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize()
+    ) {
+        CircularProgressIndicator()
+    }
 }
 
 @Composable
@@ -83,6 +120,8 @@ fun AmphibiansCard(
                 .build(),
                 contentScale = ContentScale.Crop,
                 contentDescription = "",
+                error = painterResource(id = R.drawable.ic_broken_image),
+                placeholder = painterResource(id = R.drawable.loading_img),
                 modifier = modifier.fillMaxWidth()
             )
             Text(
@@ -99,7 +138,7 @@ fun AmphibiansCard(
 @Composable
 fun AmphibiansCardScreen() {
     AmphibiansAppTheme {
-        val mockData = List(10) {AmphibiansPhoto("Amhibians", "Todo", "pata ni", "")}
+        val mockData = List(10) {AmphibiansPhoto("Amphibians", "Todo", "pasta ni", "")}
         AmphibiansListScreen(amphibiansPhotoList = mockData)
     }
 }
